4b8f0f10e93b67e042e06ff6028e159ce8e81c39
Michael <michael.ferretti@outlook.com>
2017-07-07 21:35:13 -0400

    More routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,16 @@ Rails.application.routes.draw do
 
   # Blog
   get '/blog' => 'posts#index'
-
+  post '/blog' => 'posts#create'
   get '/blog/:id' => 'posts#show'
+  patch '/blog/:id' => 'posts#update'
 
   # Project Portfolio
 
   # Admin
   get '/admin' => 'admin#index'
+  get '/admin/blog/new' => 'posts#new'
+  get '/admin/blog/:id/edit' => 'posts#edit'
+  delete '/admin/blog/:id' => 'posts#destroy'		
 
 end
